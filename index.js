0d5df5a060999baf490b3f878af082eba3747dd4
raian-shahrear <raianshahrearbd@gmail.com>
2022-10-25 21:30:44 +0600

    dataset added for coursesByCategory.json

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const port = process.env.port || 5000;
 const cors = require('cors');
 app.use(cors());
 
+
+
 app.get('/', (req, res) => {
   res.send('Server is running...');
 })
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
