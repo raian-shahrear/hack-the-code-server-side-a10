7a0bfcbfe4ce5db94c9b3fdd2687a02ff3e285c5
raian-shahrear <raianshahrearbd@gmail.com>
2022-10-25 22:28:59 +0600

    server site done

--- index.js
+++ index.js
@@ -4,13 +4,40 @@ const port = process.env.port || 5000;
 const cors = require('cors');
 app.use(cors());
 
-
+const categories = require('./Database/categories.json');
+const coursesByCategory = require('./Database/coursesByCategory.json');
+const courses = require('./Database/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Server is running...');
 })
 
-
+// display categories
+app.get('/categories', (req, res) => {
+  const displayCategories = categories.filter(c => c.isAdded === true)
+  res.send(displayCategories);
+})
+// display top enrolled courses
+app.get('/top-courses', (req, res) => {
+  const topCourses = coursesByCategory?.filter(tc => tc.isAdded === false);
+  res.send(topCourses);
+})
+// display courses by category
+app.get('/category/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const categoryCourses = coursesByCategory?.filter(cc => cc.categoryId === id && cc.isAdded === true);
+  res.send(categoryCourses);
+})
+// display single course details
+app.get('/category/course-details/:id', (req, res) => {
+  const id = req.params.id;
+  const singleCourseDetails = coursesByCategory?.find(scd => scd.courseId === id);
+  res.send(singleCourseDetails);
+})
+// display courses
+app.get('/courses', (req, res) => {
+  res.send(courses);
+})
 
 
 
