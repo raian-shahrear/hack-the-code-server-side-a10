7f9d7ceee005b50164d0fd930803200d05e9ef87
raian-shahrear <raianshahrearbd@gmail.com>
2022-10-25 14:41:24 +0600

    process.env.port and cors added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Server is running...')
+  res.send('Server is running...');
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`);
 })
